dfa930c3b07bb33288d1e8aa06a0bd40a79f1dfb
Nicholas Pilotto <nicholaspilotto@gmail.com>
2023-05-11 21:05:47 +0200

    Naming server

---
+++ naming-server/src/test/java/com/nicholaspilotto/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholaspilotto.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
